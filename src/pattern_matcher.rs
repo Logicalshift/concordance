a52404c48a9bb2c56e31495f7cb76df144de6953
Andrew Hunter <andrew@logicalshift.co.uk>
2016-08-29 15:21:54 +0100

    Add trait for matchers to use as a state machine

--- src/pattern_matcher.rs
+++ src/pattern_matcher.rs
@@ -28,4 +28,46 @@
 /// `OutputSymbol`
 ///
 pub trait PatternMatcher<InputSymbol, OutputSymbol> {
+    // type State: MatchingState<InputSymbol, OutputSymbol>;
+
+    //
+    // Creates a state that begins matching this pattern
+    //
+    // fn start(&self) -> State;
+}
+
+///
+/// Action to be taken after a matcher receives a symbol
+///
+pub enum MatchAction<OutputSymbol: ?Sized, State: ?Sized> {
+    // State is also always: MatchingState<InputSymbol, OutputSymbol> (important to know that as its how More is used)
+    //
+    // However, rust complains that InputSymbol is unused if we declare it in MatchAction and that it is undeclared if we don't
+    // If this wasn't an enum, could make it an associated type? But this is an enum so we can't do that.
+    // Ie, MatchAction<InputSymbol, OutputSymbol, State: MatchingState<InputSymbol, OutputSymbol>> = Nope (InputSymbol is unused apparently even though it's required which seems to me to mean it's used)
+    // and also MatchAction<OutputSymbol, State: MatchingState<DontCare, OutputSymbol> = Nope (DontCare is not declared)
+
+    /// The pattern does not match
+    Reject,
+
+    /// The pattern matched a certain number of symbols (which may be fewer than were passed to the matcher)
+    Accept(usize, OutputSymbol),
+
+    /// The matcher needs more symbols to decide if the pattern matches
+    More(State)
+}
+
+///
+/// Represents a state during a pattern matching operation
+///
+pub trait MatchingState<InputSymbol, OutputSymbol: ?Sized> {
+    ///
+    /// Matches the next symbol
+    ///
+    fn next(self, symbol: InputSymbol) -> MatchAction<OutputSymbol, Self>;
+
+    ///
+    /// There are no more symbols available (this can only return `Reject` or `Accept`)
+    ///
+    fn finish(self) -> MatchAction<OutputSymbol, Self>;
 }
