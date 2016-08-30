3534ea188c93ec1716c31071bfb9e9e36d44d084
Andrew Hunter <andrew@logicalshift.co.uk>
2016-08-30 11:46:53 +0100

    Can make the DFA compiler work using annoying references
    Arrghle, rust really likes to make ‘pick a type’ into a deep logic puzzle due to its surprisingly limited type inference system. (These aren’t situations where it’s ambiguous, these are situations where Rust is just dumb)

--- src/dfa_compiler.rs
+++ src/dfa_compiler.rs
@@ -26,14 +26,25 @@ use super::state_machine::*;
 ///
 /// Builds a deterministic finite automaton from a NDFA
 ///
-pub struct DfaCompiler<InputSymbol: PartialOrd, OutputSymbol, DfaType, Builder: DfaBuilder<InputSymbol, OutputSymbol, DfaType>, Ndfa: StateMachine<InputSymbol, OutputSymbol>> {
-    ndfa: Ndfa,
-    builder: Builder
+pub struct DfaCompiler<'a, InputSymbol: PartialOrd+'a, OutputSymbol: 'a, DfaType: 'a> {
+    //
+    // Would like to do this without the references and weird lifetime stuff. There's a bunch of limitations about what you can do
+    // when calling references to traits vs concrete types that make designing rust code very annoying. However, if you have a struct
+    // declared thusly: struct Foo<Bar, Quux: Baz<Bar>> and only use a field of type Quux then it'll moan that 'Bar' is unused, which
+    // is incorrect (it's used wherever Quux is used by definition), so you have to declare Foo<Bar> and use a reference to Baz<Bar>
+    // which means a lot of code won't work due to the whole mess around things needing to be sized.
+    //
+    // This basically means we can't declare ndfa or builder here using their real types. PhantomData also doesn't work because rust
+    // isn't smart enough to work out that if it knows the type of the state machine it also knows what InputSymbol and OutputSymbol
+    // are.
+    //
+
+    ndfa: &'a StateMachine<InputSymbol, OutputSymbol>,
+    builder: &'a mut DfaBuilder<InputSymbol, OutputSymbol, DfaType>,
 }
 
-impl<InputSymbol: PartialOrd, OutputSymbol, DfaType, Builder: DfaBuilder<InputSymbol, OutputSymbol, DfaType>, Ndfa: StateMachine<InputSymbol, OutputSymbol>>
-    DfaCompiler<InputSymbol, OutputSymbol, DfaType, Builder, Ndfa> {
-    pub fn new(ndfa: Ndfa, builder: Builder) -> Self {
+impl<'a, InputSymbol: PartialOrd+'a, OutputSymbol: 'a, DfaType: 'a> DfaCompiler<'a, InputSymbol, OutputSymbol, DfaType> {
+    pub fn new(ndfa: &'a StateMachine<InputSymbol, OutputSymbol>, builder: &'a mut DfaBuilder<InputSymbol, OutputSymbol, DfaType>) -> Self {
         DfaCompiler { ndfa: ndfa, builder: builder }
     }
 }
