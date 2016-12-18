dd5831d270e029b3a3f7b2eee6085a6c071e68d2
Andrew Hunter <andrew@logicalshift.co.uk>
2016-12-18 15:03:39 +0000

    Add a way to create the tagged stream from an untagged stream

--- src/tagged_stream.rs
+++ src/tagged_stream.rs
@@ -20,12 +20,55 @@
 //! A tagged stream recursively applies tags to regions of an input stream. A stream consisting of a single tag forms a tree,
 //! so this stream type is useful for representing the parsed represention of an input.
 //!
+//! The simplest form of tagged stream is one that's just a set of untagged symbols. This can be created using the `TaggedStream::from_reader`
+//! method.
+//!
+//! ```
+//! # use concordance::*;
+//! #[derive(Ord, PartialOrd, Eq, PartialEq, Clone)]
+//! enum SomeTag {
+//!     Identifier
+//! };
+//!
+//! let simple_tagged : TaggedStream<char, SomeTag> = TaggedStream::from_reader(&mut "Hello, world".read_symbols());
+//! ```
+//!
+
+use super::symbol_reader::*; 
 
 ///
-/// Represents a symbol in a tagged stream
+/// Represents a symbol in a tagged stream.
+///
+/// A symbol can either be a sequence of untagged base symbols, or it can be a tag (which itself can contain any number of tagged symbol)
 ///
 #[derive(Clone, PartialEq, Eq, Debug)]
 pub enum TagSymbol<Base: Clone, Tag: Clone> {
     Untagged(Vec<Base>),
-    Tagged(Tag, Vec<TagSymbol<Base, Tag>>)
+    Tagged(Tag, Vec<TaggedStream<Base, Tag>>)
+}
+
+///
+/// Represents a stream of tagged symbols
+///
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub struct TaggedStream<Base: Clone, Tag: Clone> {
+    /// The data in this stream
+    data: Vec<TagSymbol<Base, Tag>>
+}
+
+impl<Base: Clone, Tag: Clone> TaggedStream<Base, Tag> {
+    ///
+    /// Creates a basic tagged stream from a source of the base symbol
+    ///
+    pub fn from_reader(reader: &mut SymbolReader<Base>) -> TaggedStream<Base, Tag> {
+        // Read from the reader into an array
+        let mut symbols = vec![];
+
+        while let Some(next_symbol) = reader.next_symbol() {
+            symbols.push(next_symbol);
+        }
+
+        // Generate a simple tagged stream from the result
+        TaggedStream { data: vec![ TagSymbol::Untagged(symbols) ]}
+    }
 }
